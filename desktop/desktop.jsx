b827fc266df966e8e1f7c76eb6ec8d3700e0cf2a
David Francisco <dmfrancisc@gmail.com>
2015-03-19 21:08:40 +0000

    Add saving capabilities to the FrontMatter helper

--- desktop/desktop.jsx
+++ desktop/desktop.jsx
@@ -1,36 +1,62 @@
 /*jshint esnext:true, browserify:true, unused:vars */
 'use strict';
 
+import async from 'async';
+import fs from 'fs';
 import React from 'react';
 import FrontMatter from './helpers/front-matter';
 import CardStore from './flow/card-store';
 import Router from './components/router-component.jsx';
+import data from '../seeds/data';
 
 let startUrl = "/boards/3551dbaf6a52a/";
-let cardFilenamePattern = "./seeds/**/*.md";
+
+const Config = {
+  documents: {
+    location: "./seeds",
+    ext: "md",
+    filenamePattern: "./seeds/**/*.md"
+  },
+  data: {
+    location: "./seeds/data.json"
+  }
+};
 
 // Do persistence by reading and saving data to the filesystem
 CardStore.sync = function(method, model) {
   let methods = {
-    read(model, resolve, reject) {
-      FrontMatter.parseAllFiles(cardFilenamePattern, function(err, data) {
+    read(resolve, reject) {
+      FrontMatter.parseAllFiles(Config.documents.filenamePattern, function(err, data) {
         resolve(data);
       });
     },
-    create(model, resolve, reject) {
+    create(resolve, reject, model) {
+      async.parallel([
+        function() {
+          let path = `${ Config.documents.location }/${ model.id }.${ Config.documents.ext }`;
+          let newDocument = { id: model.id, content: model.content };
+          FrontMatter.writeFile(path, newDocument);
+        },
+        function() {
+          let copy = Object.assign({}, data);
+          copy.cards = []; // We'll re-add with only the id and listId properties
+          data.cards.forEach(({ id, listId }) => copy.cards.push({ id, listId }));
+          let str = JSON.stringify(copy, null, '\t'); // \t for pretty-print
+          fs.writeFile(Config.data.location, str);
+        }
+      ], resolve);
+    },
+    update(resolve, reject, model) {
       reject({ msg: "Not implemented" }); // TODO
     },
-    update(model, resolve, reject) {
-      reject({ msg: "Not implemented" }); // TODO
-    },
-    delete(model, resolve, reject) {
+    delete(resolve, reject, model) {
       reject({ msg: "Not implemented" }); // TODO
     }
   };
 
   if (methods[method]) {
     return new Promise((resolve, reject) => {
-      methods[method](model, resolve, reject);
+      methods[method](resolve, reject, model);
     });
   }
 };
--- helpers/front-matter.js
+++ helpers/front-matter.js
@@ -4,24 +4,26 @@
 import async from 'async';
 import fs from 'fs';
 import glob from 'glob';
-import fm from 'front-matter';
+import matter from 'gray-matter';
 
 /*
- * Front Matter
+ * Server-side Front Matter Helper
  *
- * Utility methods to parse content with YAML Front Matter blocks.
- * Extends the `front-matter` gem by adding methods to parse files and
- * customizes the resulting object.
+ * Utility methods to parse and write content with YAML Front Matter blocks.
+ * Extends the `gray-matter` gem by adding methods to asynchronously parse files
+ * and customizes the resulting object. This code is intended to be used in a
+ * server or desktop app.
  */
 let FrontMatter = {
   // Parse the data string into an `attributes` property (contains the extracted
   // YAML) and `body` (contains the string contents below the yaml separators).
-  parse(data) {
-    let parsedData = fm(data);
+  // @param {String} str - The string to parse.
+  parse(str) {
+    let parsedData = matter(str);
 
-    // Data massage: move YAML attributes to the root; rename `body` to `content`
-    let result = parsedData.attributes || {};
-    result.content = parsedData.body;
+    // Data massage: move YAML attributes to the root; ignore `original` property
+    let result = parsedData.data || {};
+    result.content = parsedData.content;
 
     return result;
   },
@@ -30,10 +32,11 @@ let FrontMatter = {
       if (err) throw err;
 
       let filename = filepath.split("/").slice(-1)[0];
+      let extension = filename.split(".").slice(-1)[0];
       let parsedData = FrontMatter.parse(data);
 
       // Use the filename if an ID attribute was not specified.
-      parsedData.id = parsedData.id || filename.replace(".md", "");
+      parsedData.id = parsedData.id || filename.replace(`.${extension}`, "");
 
       callback(err, parsedData);
     });
@@ -46,6 +49,16 @@ let FrontMatter = {
 
       async.map(filepaths, this.parseFile, callback);
     });
+  },
+  // @param {Object} data - An object with the front matter to stringify
+  // and a property `content` with the content string to append
+  stringify(data) {
+    let attributes = Object.assign({}, data);
+    delete attributes.content;
+    return matter.stringify(data.content, attributes);
+  },
+  writeFile(filepath, data, callback) {
+    fs.writeFile(filepath, this.stringify(data), callback);
   }
 };
 
